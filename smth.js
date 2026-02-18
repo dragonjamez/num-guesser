ec9431710caf6125cc46e310aea691ca9267d180
Jamey <jamey_brandon@icloud.com>
2026-02-18 13:14:27 -0500

    hi

--- smth.js
+++ smth.js
@@ -1,1 +1,13 @@
-smth
+let randomNumber = Math.floor(Math.random() * 10) + 1; 
+x = prompt("Guess a number between 1 and 10");
+xy = 1
+while (x != randomNumber) {
+    if (isNaN(x))
+        x = prompt("no num u idiot")
+    if (x > randomNumber)
+        x = prompt("lower")
+    if (x < randomNumber)
+        x = prompt("higher")
+    xy++
+}
+alert("well you finally got it after " + xy + " tries")
